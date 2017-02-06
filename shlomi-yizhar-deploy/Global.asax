7356f97cee53e7fb753fdb356a7770a345764d01
Shlomi Moshe <shlomimo@matrix.co.il>
2017-02-06 08:59:55 +0200

    sfdsd

---
+++ shlomi-yizhar-deploy/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="shlomi_yizhar_deploy.MvcApplication" Language="C#" %>
---
+++ shlomi-yizhar-deploy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(shlomi_yizhar_deploy.Startup))]
+namespace shlomi_yizhar_deploy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
